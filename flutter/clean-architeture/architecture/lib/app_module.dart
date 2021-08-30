db6c54819f1521f000386e80f8a72a0247b226a6
djeisonCassimiro <djeison.dev@gmail.com>
2021-08-30 07:28:20 -0300

    Construção dos testes de autenticação

--- flutter/clean-architeture/architecture/lib/app_module.dart
+++ flutter/clean-architeture/architecture/lib/app_module.dart
@@ -5,18 +5,27 @@ import 'package:architecture/modules/architecture/external/custom_dio/custom_dio
 import 'package:architecture/modules/architecture/external/datasource/post_datasource_impl.dart';
 import 'package:architecture/modules/architecture/presenter/post/post_bloc.dart';
 import 'package:architecture/modules/architecture/presenter/post/post_page.dart';
-import 'package:dio/dio.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+
+import 'modules/architecture/data/repositories/auth_repository_impl.dart';
+import 'modules/architecture/domain/usercases/impl/auth_usercase_impl.dart';
+import 'modules/architecture/external/datasource/auth_datasource_impl.dart';
 
 class AppModule extends Module {
 
   @override
   List<Bind<Object>> get binds => [
     Bind.singleton((i) => CustomDio()),
+    /// Binds referente Post
     Bind.singleton((i) => PostDatasourceImpl(i())),
     Bind.singleton((i) => PostRepositoryImpl(i())),
     Bind.singleton((i) => PostUsercaseImpl(i())),
     Bind.singleton((i) => PostBloc(i())),
+    /// Binds referente a autenticação
+    Bind.singleton((i) => AuthDatasourceImpl(i())),
+    Bind.singleton((i) => AuthRepositoryImpl(i())),
+    Bind.singleton((i) => AuthUsercaseImpl(i())),
+    // Bind.singleton((i) => AuthBloc(i())),
   ];
 
   @override
