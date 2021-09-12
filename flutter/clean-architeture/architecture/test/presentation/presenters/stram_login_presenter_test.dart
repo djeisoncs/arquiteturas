905480ec002709b4c7487933c6ecc9b3e12a8f73
djeisonCassimiro <djeison.dev@gmail.com>
2021-09-12 13:06:52 -0300

    ensure StremLoginPresenter emits null if email is valid

--- flutter/clean-architeture/architecture/test/presentation/presenters/stram_login_presenter_test.dart
+++ flutter/clean-architeture/architecture/test/presentation/presenters/stram_login_presenter_test.dart
@@ -45,4 +45,12 @@ void main() {
     sut.validateEmail(email);
     sut.validateEmail(email);
   });
+
+  test('Should emit null if validation email succeeds', () {
+    sut.emailErrorStream.listen(expectAsync1((error) => expect(error, null)));
+    sut.isFormValidStream.listen(expectAsync1((isValid) => expect(isValid, false)));
+
+    sut.validateEmail(email);
+    sut.validateEmail(email);
+  });
 }
