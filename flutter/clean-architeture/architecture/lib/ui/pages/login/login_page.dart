b44245a8b8c2393bc5e40869134e8fe6835010c3
djeisonCassimiro <djeison.dev@gmail.com>
2021-09-12 01:35:20 -0300

    move LoginButton to its own file

--- flutter/clean-architeture/architecture/lib/ui/pages/login/login_page.dart
+++ flutter/clean-architeture/architecture/lib/ui/pages/login/login_page.dart
@@ -61,16 +61,7 @@ class _LoginPageState extends State<LoginPage> {
                             ),
                             child: PasswordInput(),
                           ),
-                          StreamBuilder<bool>(
-                              stream: widget.presenter.isFormValidStream,
-                              builder: (context, snapshot) {
-                                return ElevatedButton(
-                                  onPressed: snapshot.data == true
-                                      ? widget.presenter.auth
-                                      : null,
-                                  child: Text("Entrar".toUpperCase()),
-                                );
-                              }),
+                          LoginButton(),
                           TextButton.icon(
                             onPressed: () {},
                             icon: Icon(Icons.person),
