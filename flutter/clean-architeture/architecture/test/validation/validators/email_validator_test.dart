14b094a5e0d87002269eee23717a815e099b1870
djeisonCassimiro <djeison.dev@gmail.com>
2021-09-12 22:29:56 -0300

    ensure EmailValidation returns null if email is empty

---
+++ flutter/clean-architeture/architecture/test/validation/validators/email_validator_test.dart
@@ -0,0 +1,29 @@
+
+
+import 'package:architecture/validation/protocols/filed_validation.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+class EmailValidation implements FieldValidation {
+  final String field;
+
+  EmailValidation(this.field);
+
+  @override
+  String validate(String value) {
+    return null;
+  }
+}
+void main() {
+
+  test('Should return null if email is empty', () {
+    final sut = EmailValidation('any_field');
+
+    expect(sut.validate(''), null);
+  });
+
+  test('Should return null if email is null', () {
+    final sut = EmailValidation('any_field');
+
+    expect(sut.validate(null), null);
+  });
+}
