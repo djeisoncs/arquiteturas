1be8cb9bbc13fe4f369ccf43badf2be1886be1a6
djeisonCassimiro <djeison.dev@gmail.com>
2021-09-13 22:12:46 -0300

    add GetxLoginPresenter

--- flutter/clean-architeture/architecture/lib/main/factories/pages/login/login_presenter_factory.dart
+++ flutter/clean-architeture/architecture/lib/main/factories/pages/login/login_presenter_factory.dart
@@ -2,8 +2,15 @@ import '../../../../presentetion/presenters/presenter.dart';
 import '../../../../ui/pages/login/login.dart';
 import '../../factories.dart';
 
-LoginPresenter makeLoginPresenter() {
+LoginPresenter makeStreamLoginPresenter() {
   return StreamLoginPresenter(
+      authentication: makeRemoteAuthentication(),
+      validation: makeLoginValidation()
+  );
+}
+
+LoginPresenter makeGetxLoginPresenter() {
+  return GetxLoginPresenter(
       authentication: makeRemoteAuthentication(),
       validation: makeLoginValidation()
   );
