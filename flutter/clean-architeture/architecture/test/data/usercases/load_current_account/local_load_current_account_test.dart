0bf647e0d159a113e0c8899a366efe41dfacee62
djeisonCassimiro <djeison.dev@gmail.com>
2021-09-15 20:24:25 -0300

    ensure LocalLoadCurrentAccount returns an account on sucess

--- flutter/clean-architeture/architecture/test/data/usercases/load_current_account/local_load_current_account_test.dart
+++ flutter/clean-architeture/architecture/test/data/usercases/load_current_account/local_load_current_account_test.dart
@@ -1,32 +1,54 @@
+
+import 'package:architecture/domain/usecases/load_current_account.dart';
+import 'package:faker/faker.dart';
 import 'package:meta/meta.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 
-class LocalLoadCurrentAccount {
+import 'package:architecture/domain/entities/account_entity.dart';
+
+class LocalLoadCurrentAccount implements LoadCurrentAccount {
   final FeatchSecureCacheStore featchSecureCacheStore;
 
   LocalLoadCurrentAccount({@required this.featchSecureCacheStore});
 
   @override
-  Future<void> load() async {
-    await featchSecureCacheStore.fetchSecure('token');
+  Future<AccountEntity> load() async {
+    final token = await featchSecureCacheStore.fetchSecure('token');
+    return AccountEntity(token);
   }
 }
 
 abstract class FeatchSecureCacheStore {
-  Future<void> fetchSecure(String key);
+  Future<String> fetchSecure(String key);
 }
 
 class FeatchSecureCacheStoreSpy extends Mock implements FeatchSecureCacheStore {}
 
 void main() {
+  FeatchSecureCacheStoreSpy featchSecureCacheStore;
+  LocalLoadCurrentAccount sut;
+  String token;
+
+  void mockFetchSecure() {
+    when(featchSecureCacheStore.fetchSecure(any)).thenAnswer((_) async => token);
+  }
+  setUp(() {
+    featchSecureCacheStore = FeatchSecureCacheStoreSpy();
+    sut = LocalLoadCurrentAccount(featchSecureCacheStore: featchSecureCacheStore);
+    token = Faker().guid.guid();
+    mockFetchSecure();
+  });
 
   test('Should call FetchSecureCacheStorage with correct value', () async {
-    final featchSecureCacheStore = FeatchSecureCacheStoreSpy();
-    final sut = LocalLoadCurrentAccount(featchSecureCacheStore: featchSecureCacheStore);
-
     await sut.load();
 
     verify(featchSecureCacheStore.fetchSecure('token'));
+  });
+
+  test('Should return an AccountEntity', () async {
+    final account = await sut.load();
+
+    expect(account, AccountEntity(token));
   });
 }
