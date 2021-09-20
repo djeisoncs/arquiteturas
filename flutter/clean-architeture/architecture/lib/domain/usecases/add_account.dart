f1f8e086e4599018abc36cef894be1769702904c
djeisonCassimiro <djeison.dev@gmail.com>
2021-09-20 00:01:53 -0300

    add AddAccount use case

---
+++ flutter/clean-architeture/architecture/lib/domain/usecases/add_account.dart
@@ -0,0 +1,28 @@
+
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+import '../entities/account_entity.dart';
+
+abstract class AddAccount {
+
+  Future<AccountEntity> add(AddAccountParams params);
+}
+
+// ignore: must_be_immutable
+class AddAccountParams extends Equatable {
+  final String name;
+  final String email;
+  final String password;
+  final String passwordConfirmation;
+
+  AddAccountParams({
+    @required this.name,
+    @required this.email,
+    @required this.password,
+    @required this.passwordConfirmation
+  });
+
+  @override
+  List<Object> get props => [name, email, password, passwordConfirmation];
+}
