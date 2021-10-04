308e717e027a5a9105e153ca77124b4df599b5ee
djeisonCassimiro <djeison.dev@gmail.com>
2021-10-03 23:09:14 -0300

    change Login composition to use MinLengthValidation on password

--- flutter/clean-architeture/architecture/lib/validation/validators/min_length_validation.dart
+++ flutter/clean-architeture/architecture/lib/validation/validators/min_length_validation.dart
@@ -1,10 +1,11 @@
+import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
 
 import '../../presentetion/protocols/protocols.dart';
 
 import '../protocols/protocols.dart';
 
-class MinLengthValidation implements FieldValidation {
+class MinLengthValidation extends Equatable implements FieldValidation  {
   final String field;
   final int size;
 
@@ -14,4 +15,7 @@ class MinLengthValidation implements FieldValidation {
   ValidationError validate(String value) {
     return value != null && value.length >= size ? null : ValidationError.invalidField;
   }
+
+  @override
+  List<Object> get props => [field, size];
 }
