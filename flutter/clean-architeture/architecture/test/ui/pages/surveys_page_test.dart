410aa9d53475d26c3327438314f83bf0d770c475
djeisoncs <djeison.cassimiro@gmail.com>
2021-10-23 00:16:13 -0300

    move Load Surveys Page setup to a helper method

--- flutter/clean-architeture/architecture/test/ui/pages/surveys_page_test.dart
+++ flutter/clean-architeture/architecture/test/ui/pages/surveys_page_test.dart
@@ -9,20 +9,41 @@ class SurveysPresenterSpy extends Mock implements SurveysPresenter {}
 void main() {
   SurveysPresenterSpy presenter;
 
-  setUp(() {
+  Future<void> loadPage(WidgetTester tester) async {
     presenter = SurveysPresenterSpy();
-  });
 
-  testWidgets('Should call LoadSurveys on page load', (WidgetTester tester) async {
     final surveysPage = GetMaterialApp(
       initialRoute: '/surveys',
       getPages: [
         GetPage(name: '/surveys', page: () => SurveysPage(presenter))
       ],
     );
-
     await tester.pumpWidget(surveysPage);
+  }
 
+
+  testWidgets('Should call LoadSurveys on page load', (WidgetTester tester) async {
+    await loadPage(tester);
     verify(presenter.loadData()).called(1);
   });
+
+  // testWidgets('Shold handle loading correctly', (WidgetTester tester) async {
+  //   await loadPage(tester);
+  //
+  //   isLoadingController.add(true);
+  //   await tester.pump();
+  //
+  //   expect(find.byType(CircularProgressIndicator), findsOneWidget);
+  // });
+
+  // testWidgets('Shold hide loading', (WidgetTester tester) async {
+  //   await loadPage(tester);
+  //
+  //   isLoadingController.add(true);
+  //   await tester.pump();
+  //   isLoadingController.add(false);
+  //   await tester.pump();
+  //
+  //   expect(find.byType(CircularProgressIndicator), findsNothing);
+  // });
 }
