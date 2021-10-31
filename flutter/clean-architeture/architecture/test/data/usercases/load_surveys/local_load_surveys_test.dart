79dc922ebd96b4db7dfef44aa1667210de5a3f46
djeisoncs <djeison.cassimiro@gmail.com>
2021-10-31 01:18:08 -0300

    ensure LocalLoadSurveys calls CaheStorage.save with correct values

--- flutter/clean-architeture/architecture/test/data/usercases/load_surveys/local_load_surveys_test.dart
+++ flutter/clean-architeture/architecture/test/data/usercases/load_surveys/local_load_surveys_test.dart
@@ -187,4 +187,49 @@ void main() {
       verify(fetchCacheStorage.delete('surveys')).called(1);
     });
   });
+
+  group('save', () {
+    LocalLoadSurveys sut;
+    FetchCacheStorageSpy cacheStorage;
+    List<SurveyEntity> surveys;
+
+
+    List<SurveyEntity> mockSurveys() => [
+      SurveyEntity(id: faker.guid.guid(), question: faker.randomGenerator.string(50), dateTime: DateTime.utc(2020, 2, 2), didAnswer: true),
+      SurveyEntity(id: faker.guid.guid(), question: faker.randomGenerator.string(50), dateTime: DateTime.utc(2018, 12, 20), didAnswer: false)
+    ];
+
+    PostExpectation mockCallsFetch () => when(cacheStorage.fetch(any));
+
+
+    void mockFetchError() => mockCallsFetch().thenThrow(Exception());
+
+    setUp(() {
+      cacheStorage = FetchCacheStorageSpy();
+      sut = LocalLoadSurveys(
+          cacheStorage: cacheStorage
+      );
+
+      surveys = mockSurveys();
+    });
+
+    test('Should call cacheStorage with correct values', () async {
+      final list = [{
+        'id': surveys[0].id,
+        'question': surveys[0].question,
+        'date': '2020-02-02T00:00:00.000Z',
+        'didAnswer': 'true',
+      },
+      {
+        'id': surveys[1].id,
+        'question': surveys[1].question,
+        'date': '2018-12-20T00:00:00.000Z',
+        'didAnswer': 'false',
+      }];
+
+      await sut.save(surveys);
+
+      verify(cacheStorage.save(key: 'surveys', value: list)).called(1);
+    });
+  });
 }
