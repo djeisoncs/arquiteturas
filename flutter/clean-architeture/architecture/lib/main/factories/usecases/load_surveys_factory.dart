c3ba7a3ad7b0887a3d87b3089d35ca68db246c08
djeisoncs <djeison.cassimiro@gmail.com>
2021-11-04 21:35:36 -0300

    add RemoteLoadSurveysWithLocalFallback factory

--- flutter/clean-architeture/architecture/lib/main/factories/usecases/load_surveys_factory.dart
+++ flutter/clean-architeture/architecture/lib/main/factories/usecases/load_surveys_factory.dart
@@ -1,8 +1,19 @@
 import '../../../data/usecases/usecases.dart';
 import '../../../domain/usecases/usercases.dart';
+import '../../composites/composites.dart';
 import '../factories.dart';
 
 LoadSurveys makeRemoteLoadSurveys() => RemoteLoadSurveys(
     httpClient: makeAuthorizeHttpClientDecorator(),
     url: makeApiUrl('surveys')
 );
+
+LoadSurveys makeLocalLoadSurveys() => LocalLoadSurveys(
+  cacheStorage: makeLocalStorageAdapter()
+);
+
+LoadSurveys makeRemoteLoadSurveysWithLocalFallback() =>
+    RemoteLoadSurveysWithLocalFallback(
+        local: makeLocalLoadSurveys(),
+        remote: makeRemoteLoadSurveys()
+    );
