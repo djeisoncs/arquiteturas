3718f3d8d17ada7ca12cb2845f9ef9253253712e
djeisoncs <djeison.cassimiro@gmail.com>
2021-11-04 22:06:37 -0300

    exclusão de arquivos

--- flutter/clean-architeture/architecture/lib/modules/architecture/data/datasources/post_datasource.dart
+++
@@ -1,9 +0,0 @@
-
-import 'package:architecture/modules/architecture/data/model/PostModel.dart';
-
-abstract class PostDatasource {
-
-
-  Future<List<PostModel>> getPosts();
-  Future<PostModel> creatPosts(PostModel postModel);
-}
