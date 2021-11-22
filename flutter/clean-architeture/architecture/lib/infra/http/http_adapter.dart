2f31fd901f6437ea0ade2124f0d73fbff06d6dc5
djeisoncs <djeison.cassimiro@gmail.com>
2021-11-22 00:03:33 -0300

    add put HttpAdapter

--- flutter/clean-architeture/architecture/lib/infra/http/http_adapter.dart
+++ flutter/clean-architeture/architecture/lib/infra/http/http_adapter.dart
@@ -18,11 +18,17 @@ class HttpAdapter implements HttpClient {
     });
     final jsonBody = body != null ? jsonEncode(body) : null;
     var response = Response('', 500);
+    Future<Response> futureResponse;
     try {
       if (method == 'post') {
-        response = await client.post(Uri.parse(url), headers: defaultHeaders, body: jsonBody).timeout(Duration(seconds: 10));
+        futureResponse = client.post(Uri.parse(url), headers: defaultHeaders, body: jsonBody);
       } else if (method == 'get') {
-        response = await client.get(Uri.parse(url), headers: defaultHeaders).timeout(Duration(seconds: 10));
+        futureResponse = client.get(Uri.parse(url), headers: defaultHeaders);
+      } else if (method == 'put') {
+        futureResponse = client.put(Uri.parse(url), headers: defaultHeaders, body: jsonBody);
+      }
+      if (futureResponse != null) {
+        response = await futureResponse.timeout(Duration(seconds: 10));
       }
     } catch(error) {
       throw HttpError.serverError;
